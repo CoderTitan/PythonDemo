ab5bf2300a4972f91fa0ad35139a1b4a6c0534e3
CoderTitan <quanjunt@163.com>
2018-04-16 17:12:15 +0800

    运算符和条件语句

---
+++ PythonStudy/1-基础教程/3-各种语句.py
@@ -0,0 +1,43 @@
+
+# 1.条件语句
+'''
+# if-else语句
+if 2 > 3:
+    print('真的')
+else:
+    print("假的")
+
+# if-elif语句
+a = 3
+if (a == 1):
+    print(1)
+elif (a == 2):
+    print(2)
+else:
+    print(3)
+'''
+
+# 循环语句
+'''
+# 1-1. while
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+'''
+
+# for循环
+# 输出每一个字符
+for s in "char":
+    print(s)
+
+# 使用索引
+list2 = [5, 2, 4]
+for index in range(len(list2)):
+    print(list2[index])
+else:
+    print('完成')
+
+
+
+
